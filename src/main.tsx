34adbf8a5b24ccd184d2b91fa53320ae89c5325e
Bojan Jagetic <jagetic.bojan@gmail.com>
2025-06-04 00:31:08 +0200

    feat: testing if its tv useragent

--- src/main.tsx
+++ src/main.tsx
@@ -6,10 +6,15 @@ import { Provider } from "react-redux";
 import { store } from "./store/index.ts";
 import { init } from "@noriginmedia/norigin-spatial-navigation";
 
+const isTV =
+   /Tizen|Web0S|SmartTV|TV/.test(navigator.userAgent) || 
+   (navigator.userAgent.includes("SMART-TV") || navigator.userAgent.includes("HbbTV"));
+
+   console.log('Navigator agetn ', navigator.userAgent)
 init({
    debug: false,
    visualDebug: false,
-   nativeMode: false,
+   nativeMode: isTV,
    throttle: 50,
    useGetBoundingClientRect: true,
    shouldFocusDOMNode: true,
